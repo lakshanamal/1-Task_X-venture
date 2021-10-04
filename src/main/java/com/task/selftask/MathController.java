754d68d35b50ddac28ad4448755b4bc010a78deb
LakshanAmal <lakshanamal100@gmail.com>
2021-10-04 19:28:39 +0530

    Update to json response

--- src/main/java/com/task/selftask/MathController.java
+++ src/main/java/com/task/selftask/MathController.java
@@ -1,10 +1,14 @@
 package com.task.selftask;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("api")
@@ -13,9 +17,11 @@ public class MathController {
     private MathService mathService;
 
     @PostMapping("/add")
-    public Integer  addTwoNumber(@RequestBody MathModel mathModel){
+    public ResponseEntity<Map<String,Integer>>  addTwoNumber(@RequestBody MathModel mathModel){
         Integer a= mathModel.getA();
         Integer b=mathModel.getB();
-        return mathService.addTwoNumber(a,b);
+        Map<String, Integer> map = new HashMap<>();
+        map.put("answer",mathService.addTwoNumber(a,b));
+        return ResponseEntity.ok(map);
     }
 }
